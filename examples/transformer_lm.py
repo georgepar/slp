f9249709aca0e3827cce45ec87226747ef6e9521
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2019-06-16 18:36:56 +0300

    add small transformer lm training script

--- examples/transformer_lm.py
+++ examples/transformer_lm.py
@@ -1,18 +1,25 @@
 import itertools
+import torch.nn as nn
+import torch.optim as optim
+
 from torch.utils.data import DataLoader
 from torchnlp.datasets import wikitext_2_dataset
 
 from slp.config import SPECIAL_TOKENS
 from slp.data.datasets import LMDataset
 from slp.data.collators import TransformerCollator
+from slp.modules.transformer import Transformer
 from slp.data.transforms import ReplaceUnknownToken, ToTokenIds, ToTensor
-
+from slp.trainer import TransformerTrainer
 
 collate_fn = TransformerCollator(device='cpu')
 
 
 if __name__ == "__main__":
-    max_len = 256  # TODO: argparse this
+    max_len = 64  # TODO: argparse this
+    vocab_size = 5000
+    lr = 1e-4
+
     train, dev, test = wikitext_2_dataset(
         directory='data/',
         train=True,
@@ -24,7 +31,7 @@ if __name__ == "__main__":
         eos_token=SPECIAL_TOKENS.EOS.value)
     vocab = dict(zip(list(
         set(SPECIAL_TOKENS.to_list() + train + dev)),
-        itertools.count()))
+        itertools.count())[:vocab_size])
     replace_unk = ReplaceUnknownToken()
     to_token_ids = ToTokenIds(vocab)
     to_tensor = ToTensor(device='cpu')
@@ -43,3 +50,31 @@ if __name__ == "__main__":
             collate_fn=collate_fn)
 
     train_loader = create_dataloader(train)
+    dev_loader = create_dataloader(dev)
+    test_loader = create_dataloader(test)
+
+    model = Transformer(vocab_size=vocab_size,
+                        max_length=max_len,
+                        num_layers=2,
+                        hidden_size=128,
+                        num_heads=4,
+                        inner_size=512)
+
+    optimizer = optim.Adam(
+        [p for p in model.parameters() if p.requires_grad],
+        lr=lr)
+
+    trainer = TransformerTrainer(
+        model,
+        optimizer,
+        checkpoint_dir='../../checkpoints',
+        experiment_name='transformer_lm',
+        metrics=None,
+        patience=4,
+        validate_every=1,
+        accumulation_steps=2,
+        loss_fn=nn.CrossEntropyLoss(),
+        non_blocking=True,
+        device='cuda:0')
+
+    trainer.fit(train_loader, dev_loader, epochs=10)
