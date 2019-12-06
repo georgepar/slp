checked if loss computation with padding is correct

slp/modules/loss.py
@@ -7,9 +7,7 @@ class SequenceCrossEntropyLoss(nn.Module):
         self.criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
 
     def forward(self, y_pred, targets): 
-        y = y_pred
-        t = targets
+
         y_pred = y_pred.reshape(-1, y_pred.size(-1))
-        targets = targets.reshape(-1)        
-            
+        targets = targets.reshape(-1)
         return self.criterion(y_pred, targets)