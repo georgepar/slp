25aee27b34e00c6e3a01b8fb53e7bf7a2d500466
manzar96 <manoszar96@gmail.com>
2019-12-15 21:03:07 +0200

    added gradient plots

--- slp/util/plot_grads.py
+++ slp/util/plot_grads.py
@@ -27,9 +27,8 @@ def plot_grad_flow(named_parameters):
     plt.hlines(0, 0, len(ave_grads) + 1, lw=2, color="k")
     plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
     plt.xlim(left=0, right=len(ave_grads))
-    plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+    plt.ylim(bottom=0.3, top=0.02)  # zoom in on the lower gradient regions
     plt.xlabel("Layers")
     plt.ylabel("average gradient")
     plt.title("Gradient flow")
     plt.grid(True)
-
