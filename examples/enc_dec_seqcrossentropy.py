bbd7b5e84b3a8391bb8e89fb6697116157dd95fd
manzar96 <manoszar96@gmail.com>
2020-01-07 18:03:22 +0200

    enc dec implementation with sequence cross entropy loss

--- examples/enc_dec_seqcrossentropy.py
+++ examples/enc_dec_seqcrossentropy.py
@@ -346,8 +346,6 @@ for pair in pairs[:10]:
     print(pair)
 
 
-import ipdb;ipdb.set_trace()
-
 MIN_COUNT = 3    # Minimum word count threshold for trimming
 
 def trimRareWords(voc, pairs, MIN_COUNT):
@@ -381,7 +379,6 @@ def trimRareWords(voc, pairs, MIN_COUNT):
 
 # Trim voc and pairs
 pairs = trimRareWords(voc, pairs, MIN_COUNT)
-import ipdb;ipdb.set_trace()
 
 
 
@@ -445,120 +442,8 @@ print("mask:", mask)
 print("max_target_len:", max_target_len)
 
 import ipdb;ipdb.set_trace()
-
-class EncoderRNN(nn.Module):
-    def __init__(self, hidden_size, embedding, n_layers=1, dropout=0):
-        super(EncoderRNN, self).__init__()
-        self.n_layers = n_layers
-        self.hidden_size = hidden_size
-        self.embedding = embedding
-
-        # Initialize GRU; the input_size and hidden_size params are both set to 'hidden_size'
-        #   because our input size is a word embedding with number of features == hidden_size
-        self.gru = nn.GRU(hidden_size, hidden_size, n_layers,
-                          dropout=(0 if n_layers == 1 else dropout), bidirectional=True)
-
-    def forward(self, input_seq, input_lengths, hidden=None):
-        # Convert word indexes to embeddings
-        embedded = self.embedding(input_seq)
-        # Pack padded batch of sequences for RNN module
-        packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
-        # Forward pass through GRU
-        outputs, hidden = self.gru(packed, hidden)
-        # Unpack padding
-        outputs, _ = torch.nn.utils.rnn.pad_packed_sequence(outputs)
-        # Sum bidirectional GRU outputs
-        outputs = outputs[:, :, :self.hidden_size] + outputs[:, : ,self.hidden_size:]
-        # Return output and final hidden state
-        return outputs, hidden
-
-# Luong attention layer
-class Attn(torch.nn.Module):
-    def __init__(self, method, hidden_size):
-        super(Attn, self).__init__()
-        self.method = method
-        if self.method not in ['dot', 'general', 'concat']:
-            raise ValueError(self.method, "is not an appropriate attention method.")
-        self.hidden_size = hidden_size
-        if self.method == 'general':
-            self.attn = torch.nn.Linear(self.hidden_size, hidden_size)
-        elif self.method == 'concat':
-            self.attn = torch.nn.Linear(self.hidden_size * 2, hidden_size)
-            self.v = torch.nn.Parameter(torch.FloatTensor(hidden_size))
-
-    def dot_score(self, hidden, encoder_output):
-        return torch.sum(hidden * encoder_output, dim=2)
-
-    def general_score(self, hidden, encoder_output):
-        energy = self.attn(encoder_output)
-        return torch.sum(hidden * energy, dim=2)
-
-    def concat_score(self, hidden, encoder_output):
-        energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0), -1, -1), encoder_output), 2)).tanh()
-        return torch.sum(self.v * energy, dim=2)
-
-    def forward(self, hidden, encoder_outputs):
-        # Calculate the attention weights (energies) based on the given method
-        if self.method == 'general':
-            attn_energies = self.general_score(hidden, encoder_outputs)
-        elif self.method == 'concat':
-            attn_energies = self.concat_score(hidden, encoder_outputs)
-        elif self.method == 'dot':
-            attn_energies = self.dot_score(hidden, encoder_outputs)
-
-        # Transpose max_length and batch_size dimensions
-        attn_energies = attn_energies.t()
-
-        # Return the softmax normalized probability scores (with added dimension)
-        return F.softmax(attn_energies, dim=1).unsqueeze(1)
-
-
-class LuongAttnDecoderRNN(nn.Module):
-    def __init__(self, attn_model, embedding, hidden_size, output_size, n_layers=1, dropout=0.1):
-        super(LuongAttnDecoderRNN, self).__init__()
-
-        # Keep for reference
-        self.attn_model = attn_model
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.n_layers = n_layers
-        self.dropout = dropout
-
-        # Define layers
-        self.embedding = embedding
-        self.embedding_dropout = nn.Dropout(dropout)
-        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=(0 if n_layers == 1 else dropout))
-        self.concat = nn.Linear(hidden_size * 2, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-
-        self.attn = Attn(attn_model, hidden_size)
-
-    def forward(self, input_step, last_hidden, encoder_outputs):
-        # Note: we run this one step (word) at a time
-        # Get embedding of current input word
-        embedded = self.embedding(input_step)
-        embedded = self.embedding_dropout(embedded)
-        # Forward through unidirectional GRU
-        rnn_output, hidden = self.gru(embedded, last_hidden)
-        # Calculate attention weights from the current GRU output
-        attn_weights = self.attn(rnn_output, encoder_outputs)
-        # Multiply attention weights to encoder outputs to get new "weighted sum" context vector
-        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
-        # Concatenate weighted context vector and GRU output using Luong eq. 5
-        rnn_output = rnn_output.squeeze(0)
-        context = context.squeeze(1)
-        concat_input = torch.cat((rnn_output, context), 1)
-        concat_output = torch.tanh(self.concat(concat_input))
-        # Predict next word using Luong eq. 6
-        output = self.out(concat_output)
-        output = F.softmax(output, dim=1)
-        # Return output and final hidden state
-        return output, hidden
-
-
-import ipdb;ipdb.set_trace()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-teacher_forcing_ratio=1.
+teacher_forcing_ratio=0.6
 
 def maskNLLLoss(inp, target, mask):
     nTotal = mask.sum()
@@ -596,7 +481,6 @@ def train(input_variable, lengths, target_variable, mask, max_target_len, encode
     decoder_input = decoder_input.to(device)
 
     # Set initial decoder hidden state to the encoder's final hidden state
-    # decoder_hidden = encoder_hidden[:decoder.n_layers]
     decoder_hidden = encoder_hidden[:decoder.num_layers]
 
     all_outputs = []
@@ -606,52 +490,31 @@ def train(input_variable, lengths, target_variable, mask, max_target_len, encode
     # Forward batch of sequences through decoder one time step at a time
     if use_teacher_forcing:
         for t in range(max_target_len):
-            # decoder_output, decoder_hidden = decoder(
-            #     decoder_input, decoder_hidden, encoder_outputs
-            # )
             decoder_output, decoder_hidden = decoder(
                 decoder_input, decoder_hidden
             )
 
-            """Me diko mou loss"""
+            """Gia Sequence Cross Entropy loss"""
             all_outputs.append(
-                torch.squeeze(decoder_output, dim=1))
+               torch.squeeze(decoder_output, dim=1))
 
 
             # Teacher forcing: next input is current target
-            # decoder_input = target_variable[t].view(1, -1)
-            #decoder_input = target_variable[:,t].view(1, -1)
-            #decoder_input = decoder_input.transpose(0,1)
             decoder_input = target_variable[:,t]
             decoder_input = decoder_input.unsqueeze(dim=1)
 
-            """Me maskNLLL loss"""
-            # Calculate and accumulate loss
-            # mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t])
-            curr_output = decoder_output.squeeze(dim=1)
-            # mask_loss, nTotal = maskNLLLoss(curr_output,
-            #                                 target_variable[
-            #                                                 :,t],
-            #                                 mask[t])
-            # loss += mask_loss
-            # print_losses.append(mask_loss.item() * nTotal)
-            # n_totals += nTotal
 
     else:
         for t in range(max_target_len):
-            # decoder_output, decoder_hidden = decoder(
-            #     decoder_input, decoder_hidden, encoder_outputs
-            # )
             decoder_output, decoder_hidden = decoder(
                 decoder_input, decoder_hidden
             )
 
-            """Me diko mou loss"""
+            """Gia Sequence Cross Entropy loss"""
             all_outputs.append(
                 torch.squeeze(decoder_output, dim=1))
             # No teacher forcing: next input is decoder's own current output
 
-            """Me diko mou loss"""
             current_output = torch.squeeze(decoder_output, dim=1)
             top_index = F.softmax(current_output, dim=1)
             _,topi = top_index.topk(1)
@@ -659,23 +522,10 @@ def train(input_variable, lengths, target_variable, mask, max_target_len, encode
             decoder_input= decoder_input.transpose(0,1)
             decoder_input = decoder_input.to(device)
 
-            """Me maskNLLL loss"""
-            # _, topi = decoder_output.topk(1)
-            # decoder_input = torch.LongTensor([[topi[i][0] for i in range(batch_size)]])
-            # decoder_input = decoder_input.to(device)
 
-            # Calculate and accumulate loss
-            # mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[t], mask[t])
-            # mask_loss, nTotal = maskNLLLoss(decoder_output, target_variable[
-            #                                                 :,t],
-            #                                 mask[t])
-            # loss += mask_loss
-            # print_losses.append(mask_loss.item() * nTotal)
-            # n_totals += nTotal
-
-    """Me diko mou Loss"""
+    """Gia Sequence Cross Entropy Loss"""
     all_outputs = torch.stack(all_outputs).transpose(0, 1)
-    loss = criterion(all_outputs,target_variable)
+    loss = criterion(all_outputs, target_variable)
 
 
     # Perform backpropatation
@@ -689,11 +539,7 @@ def train(input_variable, lengths, target_variable, mask, max_target_len, encode
     encoder_optimizer.step()
     decoder_optimizer.step()
 
-    """Me diko mou loss"""
     return loss.item()
-
-    """Me masknllloss"""
-    #return sum(print_losses) / n_totals
 
 
 def trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, decoder_optimizer, embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size, print_every, save_every, clip, corpus_name, loadFilename):
@@ -717,6 +563,7 @@ def trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, deco
         input_variable, lengths, target_variable, mask, max_target_len = training_batch
         input_variable = input_variable.transpose(0,1)
         target_variable = target_variable.transpose(0,1)
+
         # Run a training iteration with batch
         loss = train(input_variable, lengths, target_variable, mask, max_target_len, encoder,
                      decoder, embedding, encoder_optimizer, decoder_optimizer, batch_size, clip)
@@ -744,35 +591,6 @@ def trainIters(model_name, voc, pairs, encoder, decoder, encoder_optimizer, deco
                 'embedding': embedding.state_dict()
             }, os.path.join(directory, '{}_{}.tar'.format(iteration, 'checkpoint')))
 
-class GreedySearchDecoder(nn.Module):
-    def __init__(self, encoder, decoder):
-        super(GreedySearchDecoder, self).__init__()
-        self.encoder = encoder
-        self.decoder = decoder
-
-    def forward(self, input_seq, input_length, max_length):
-        # Forward input through encoder model
-        encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
-        # Prepare encoder's final hidden layer to be first hidden input to the decoder
-        decoder_hidden = encoder_hidden[:self.decoder.n_layers]
-        # Initialize decoder input with SOS_token
-        decoder_input = torch.ones(1, 1, device=device, dtype=torch.long) * SOS_token
-        # Initialize tensors to append decoded words to
-        all_tokens = torch.zeros([0], device=device, dtype=torch.long)
-        all_scores = torch.zeros([0], device=device)
-        # Iteratively decode one word token at a time
-        for _ in range(max_length):
-            # Forward pass through decoder
-            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden, encoder_outputs)
-            # Obtain most likely word token and its softmax score
-            decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
-            # Record token and score
-            all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
-            all_scores = torch.cat((all_scores, decoder_scores), dim=0)
-            # Prepare current token to be next decoder input (add a dimension)
-            decoder_input = torch.unsqueeze(decoder_input, 0)
-        # Return collections of word tokens and scores
-        return all_tokens, all_scores
 
 
 class MyGreedySearchDecoder(nn.Module):
@@ -889,8 +707,6 @@ embedding = nn.Embedding(voc.num_words, hidden_size)
 # if loadFilename:
 #     embedding.load_state_dict(embedding_sd)
 # Initialize encoder & decoder models
-# encoder = EncoderRNN(hidden_size, embedding, encoder_n_layers, dropout)
-# decoder = LuongAttnDecoderRNN(attn_model, embedding, hidden_size, voc.num_words, decoder_n_layers, dropout)
 
 encoder = EncoderLSTM(embedding,weights_matrix=None,hidden_size=hidden_size,
                       num_layers=encoder_n_layers,dropout=dropout,
@@ -945,5 +761,4 @@ searcher = MyGreedySearchDecoder(encoder, decoder,device)
 # Begin chatting (uncomment and run the following line to begin)
 evaluateInput(encoder,decoder,searcher, voc)
 
-import ipdb;ipdb.set_trace()
 print("end chatbot")
