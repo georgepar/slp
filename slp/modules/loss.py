51ff7ddf02c001846e42a3a70a47a2b9c9eb10ad
manzar96 <manoszar96@gmail.com>
2020-01-16 23:39:11 +0200

    updated loss

--- slp/modules/loss.py
+++ slp/modules/loss.py
@@ -7,9 +7,6 @@ class SequenceCrossEntropyLoss(nn.Module):
         self.criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
 
     def forward(self, y_pred, targets):
-        #targets = targets[:, 1:]
-        # y_pred = y_pred[1:].reshape(-1, y_pred.size(-1))
-        # targets = targets[1:].reshape(-1)
         y_pred = y_pred.contiguous().view(-1, y_pred.size(-1))
         targets = targets.contiguous().view(-1)
         return self.criterion(y_pred, targets)
