31a183c9b22aa5fed704b91c19d842e494e9acc4
manzar96 <manoszar96@gmail.com>
2020-01-16 22:41:09 +0200

    changed dummymovietriples dataset

--- slp/data/DummymovieTriples.py
+++ slp/data/DummymovieTriples.py
@@ -1,11 +1,4 @@
-import os
-import codecs
-import csv
-import unicodedata
-import re
-import numpy as np
 from torch.utils.data import Dataset
-from slp.data.transforms import *
 
 
 class DummyMovieTriples(Dataset):
@@ -14,7 +7,6 @@ class DummyMovieTriples(Dataset):
         self.triples = self.read_data(directory)
         self.transforms = transforms
 
-
     def read_data(self, directory):
         lines = open(directory).read().split("\n")[:-1]
         triplets=[]
@@ -22,8 +14,7 @@ class DummyMovieTriples(Dataset):
             triplets.append(line.split('/'))
         return triplets
 
-
-    def map(self,t):
+    def map(self, t):
         self.transforms.append(t)
         return self
 
@@ -31,16 +22,18 @@ class DummyMovieTriples(Dataset):
         return len(self.triples)
 
     def __getitem__(self, idx):
-        s1,s2,s3 = self.triples[idx]
+        s1, s2, s3 = self.triples[idx]
 
         if self.transforms is not None:
-            s1 = self.transforms(s1)
-            s2 = self.transforms(s2)
-            s3 = self.transforms(s3)
+            for t in self.transforms:
+                s1 = t(s1)
+                s2 = t(s2)
+                s3 = t(s3)
 
-        return s1,s2,s3
+        return s1, s2, s3
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     dataset = DummyMovieTriples('./data/dummy_movie_triples')
     for d in dataset:
         print(d)
