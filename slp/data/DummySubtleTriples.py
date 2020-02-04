548934b4d624ad9f94881fb28c7b4480afd0115f
manzar96 <manoszar96@gmail.com>
2020-02-04 23:02:01 +0200

    implemented 2 versions of dataloader for Subtle dataset

--- slp/data/DummySubtleTriples.py
+++ slp/data/DummySubtleTriples.py
@@ -1,15 +1,15 @@
 from torch.utils.data import Dataset
 
 
-class DummySubTriples(Dataset):
+class SubTriples1(Dataset):
     def __init__(self, directory, transforms=None, train=True):
 
-        self.triples = self.read_data(directory)
+        self.ids, self.triples = self.read_data(directory)
         self.transforms = transforms
 
     def read_data(self, directory):
         lines = open(directory).read().split("\n")[:-1][:10000]
-        triplets=[]
+        triplets = []
         ids = []
         for index, line in enumerate(lines):
             if not line == '':
@@ -23,7 +23,7 @@ class DummySubTriples(Dataset):
             if index % 6 == 0 and line == '' and (not index == 0):
                 triplets.append((u1, u2, ''))
 
-        return triplets
+        return ids, triplets
 
     def map(self, t):
         self.transforms.append(t)
@@ -33,6 +33,8 @@ class DummySubTriples(Dataset):
         return len(self.triples)
 
     def __getitem__(self, idx):
+        id = self.ids[idx]
+        #print(id)
         s1, s2, s3 = self.triples[idx]
         if self.transforms is not None:
             for t in self.transforms:
@@ -43,8 +45,50 @@ class DummySubTriples(Dataset):
         return s1, s2, s3
 
 
+class SubTriples2(Dataset):
+    def __init__(self, directory, transforms=None, train=True):
+
+        self.ids, self.triples = self.read_data(directory)
+        self.transforms = transforms
+
+    def read_data(self, directory):
+        lines = open(directory).read().split("\n")[:-1][:10000]
+        triplets = []
+        ids = []
+        for index, line in enumerate(lines):
+            if not line == '':
+                if line[0] == 'S':
+                    ids.append(line.split()[2])
+                elif line[0] == 'I':
+                    u2 = line[4:]
+                elif line[0] == 'R':
+                    u3 = line[4:]
+
+            if index % 6 == 0 and line == '' and (not index == 0):
+                triplets.append(('', u2, u3))
+
+        return ids, triplets
+
+    def map(self, t):
+        self.transforms.append(t)
+        return self
+
+    def __len__(self):
+        return len(self.triples)
+
+    def __getitem__(self, idx):
+        id = self.ids[idx]
+        s1, s2, s3 = self.triples[idx]
+        if self.transforms is not None:
+            for t in self.transforms:
+                s1 = t(s1)
+                s2 = t(s2)
+                s3 = t(s3)
+
+        return s1, s2, s3
+
 if __name__ == '__main__':
-    dataset = DummySubTriples('./data/corpus0sDialogues.txt')
+    dataset = SubTriples2('./data/corpus0sDialogues.txt')
     for d in dataset:
         print(d)
 
