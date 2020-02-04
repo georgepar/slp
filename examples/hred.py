6ef07a22a9ca84ce0fdc4e6a49136f33678025b4
manzar96 <manoszar96@gmail.com>
2020-02-04 23:00:59 +0200

    added parser for hred main

--- examples/hred.py
+++ examples/hred.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import argparse
 import os
 import torch.nn as nn
 
@@ -10,22 +11,22 @@ from torch.optim import Adam
 from slp.config.special_tokens import HRED_SPECIAL_TOKENS
 from slp.data.utils import train_test_split
 from slp.data.transforms import SpacyTokenizer, ToTokenIds, ToTensor
-from slp.data.DummySubtleTriples import DummySubTriples
-from slp.data.collators import HRED_MovieTriples_Collator
+from slp.data.DummySubtleTriples import SubTriples2
+from slp.data.collators import HRED_Collator
 from slp.util.embeddings import EmbeddingsLoader
-from slp.trainer.trainer import HREDMovieTriplesTrainer
+from slp.trainer.trainer import HREDTrainer
 from slp.modules.loss import SequenceCrossEntropyLoss
-
+from slp.modules.seq2seq.hred import HRED
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-COLLATE_FN = HRED_MovieTriples_Collator(device='cpu')
+COLLATE_FN = HRED_Collator(device='cpu')
 MAX_EPOCHS = 10
 BATCH_TRAIN_SIZE = 32
 BATCH_VAL_SIZE = 32
 
 
-def trainer_factory(embeddings, pad_index, sos_index, device=DEVICE):
+def trainer_factory(options,emb_dim,vocab_size,embeddings,  pad_index, sos_index, device=DEVICE):
 
-    model=1
+    model = HRED(options,emb_dim,vocab_size,embeddings,embeddings,device )
 
     optimizer = Adam(
         [p for p in model.parameters() if p.requires_grad],
@@ -37,7 +38,7 @@ def trainer_factory(embeddings, pad_index, sos_index, device=DEVICE):
         'loss': Loss(criterion)
     }
 
-    trainer = HREDMovieTriplesTrainer(model, optimizer, checkpoint_dir=None,
+    trainer = HREDTrainer(model, optimizer, checkpoint_dir=None,
                                       metrics=metrics, non_blocking=True,
                                       retain_graph=False, patience=5,
                                       device=device, loss_fn=criterion)
@@ -52,11 +53,11 @@ if __name__ == '__main__':
                                                       extra_tokens=
                                                       HRED_SPECIAL_TOKENS
                                                       ).load()
-
+    vocab_size = len(word2idx)
     tokenizer = SpacyTokenizer(specials=HRED_SPECIAL_TOKENS)
     to_token_ids = ToTokenIds(word2idx, specials=HRED_SPECIAL_TOKENS)
     to_tensor = ToTensor()
-    dataset = DummySubTriples('./data/corpus0sDialogues.txt', transforms=[
+    dataset = SubTriples2('./data/corpus0sDialogues.txt', transforms=[
         tokenizer, to_token_ids, to_tensor])
 
     print(dataset[0])
@@ -67,4 +68,75 @@ if __name__ == '__main__':
     sos_index = word2idx[HRED_SPECIAL_TOKENS.SOU.value]
     eos_index = word2idx[HRED_SPECIAL_TOKENS.EOU.value]
 
-    trainer = trainer_factory(embeddings, pad_index, sos_index, device=DEVICE)
+    parser = argparse.ArgumentParser(description='HRED parameter options')
+    parser.add_argument('-n', dest='name', help='enter suffix for model files')
+    parser.add_argument('-model_path', dest='model_path', default='./models', help='enter the path in which you want to store the model state')
+    parser.add_argument('-enchidden', dest='enc_hidden_size',
+                        action='store_true',
+                        default=512, help='encoder hidden size')
+    parser.add_argument('-embdrop', dest='embeddings_dropout',
+                        action='store_true',
+                        default=0, help='embeddings dropout')
+    parser.add_argument('-encembtrain', dest='enc_finetune_embeddings',
+                        action='store_true',
+                        default=False, help='encoder finetune embeddings')
+    parser.add_argument('-encnumlayers', dest='enc_num_layers',
+                        action='store_true',
+                        default=1, help='encoder number of layers')
+    parser.add_argument('-encbi', dest='enc_bidirectional',
+                        action='store_true',
+                        default=False, help='bidirectional enc')
+    parser.add_argument('-encdrop', dest='enc_dropout', action='store_true',
+                        default=0, help='encoder dropout')
+
+    parser.add_argument('-continputsize', dest='contenc_input_size',
+                        action='store_true',
+                        default=300, help='context encoder input size')
+    parser.add_argument('-conthiddensize', dest='contenc_hidden_size',
+                        action='store_true',
+                        default=512, help='context encoder hidden size')
+    parser.add_argument('-contnumlayers', dest='contenc_num_layers',
+                        action='store_true',
+                        default=1, help='context encoder number of layers')
+    parser.add_argument('-contencdrop', dest='contenc_dropout',
+                        action='store_true',
+                        default=0, help='context encoder dropout')
+    parser.add_argument('-contencbi', dest='contenc_bidirectional',
+                        action='store_true',
+                        default=False, help='bidirectional enc')
+    parser.add_argument('-contenctype', dest='contenc_rnn_type',
+                        action='store_true',
+                        default='gru', help='bidirectional enc')
+
+    parser.add_argument('-dechidden', dest='dec_hidden_size',
+                        action='store_true',
+                        default=512, help='decoder hidden size')
+    parser.add_argument('-decembtrain', dest='dec_finetune_embeddings',
+                        action='store_true',
+                        default=False, help='decoder finetune embeddings')
+    parser.add_argument('-decnumlayers', dest='dec_num_layers',
+                        action='store_true',
+                        default=1, help='decoder number of layers')
+    parser.add_argument('-decbi', dest='dec_bidirectional',
+                        action='store_true',
+                        default=False, help='bidirectional decoder')
+    parser.add_argument('-decdrop', dest='dec_dropout', action='store_true',
+                        default=0, help='decoder dropout')
+    parser.add_argument('-decmergebi', dest='dec_merge_bi',
+                        action='store_true',
+                        default='cat', help='decoder merge bidirectional '
+                                       'method')
+    parser.add_argument('-dectype', dest='dec_rnn_type',
+                        action='store_true',
+                        default='gru', help='decoder rnn type')
+
+    parser.add_argument('-bf', dest='batch_first', action='store_true',
+                        default=True, help='batch first')
+    parser.add_argument('-tf', dest='teacherforcing_ratio',
+                        action='store_true',
+                        default=0.8, help='teacher forcing ratio')
+
+    options = parser.parse_args()
+    print(options)
+    trainer = trainer_factory(options, emb_dim,vocab_size,embeddings,
+                              pad_index, sos_index, device=DEVICE)
