87772cc598592b9145a095d03558a5d731893576
danaiksez <dxezonaki@gmail.com>
2020-04-01 23:19:09 +0300

    seed words +ratings

---
+++ slp/data/seed_words.py
@@ -0,0 +1,16 @@
+depressed = {'temper':0, 'pulled':1 ,'guidance':0, 'required':0, 'agree':-1, 'motivation':-1, 'field':0, 'scary':1, 'tough':1, 'school':0,
+ 'vacation':-1, 'responsible':-1, 'study':0, 'crossroads':0??, 'wink':-1, 'competitive':-1, 'anxiousness':1, 'procrastination':1,
+ 'established':0, 'usually':0, 'knowing':0??, 'technically':0, 'med':0??, 'putting':0, 'library':0, 'prepare':-1, 'overcome':-1, 'read':0,
+ 'counselor':0, 'may':0, 'education':0, 'routine':0, 'studying':0, 'depressing':1, 'comes':0, 'pushing':1, 'research':0,
+ 'reality':0, 'alright':-1, 'brothers':0, 'road':0, 'responsibility':-1??, 'frustrating':1, 'books':0, 'problem':1, 'listening':0, 
+ 'girlfriend':0, 'fast':0, 'thoughts':???, 'mentioned':0, 'fear':1, 'reading':0, 'helping':-1, 'older':0?, 'write':0, 'answer':0, 
+'monday':1, 'friday':-1, 'lose':1, 'means':0, 'dad':0, 'phone':0, 'move':-1, 'control':1, 'mom':0, 'especially':0, 'important':-1, 'hour':0, 
+'yesterday':0, 'close':-1, 'believe':0, 'sit':0, 'job':0, 'gets':-1, 'making':-1, 'called':0, 'sleep':0, 'weeks':0, 'real':-1, 'fame':-1, 'familiar':-1, 'feared':1, 'family':0}
+
+notdepressed = {'concord':-1, 'tense':1, 'jabs':1, 'booked':-1, 'upsets':1, 'thrilled':-1, 'frustrates':1, 'anxious':1, 'weekend':-1, 'subtle':-1, 'paying':1, 'incredibly':-1, 'denying':1, 'sat':0, 'expensive':1, 'idea':0, 'wrong':1, 'afford':-1, 'psychoanalysts':0, 
+'mail':0, 'metaphoric':0, 'transactional':0, 'costly':1, 'firms':0, 'low':1, 'tired':1, 'budgeted':-1, 'pauses':1, 'taxing':1,
+'excited':1, 'kids':0, 'symbolize':0, 'relationships':0, 'money':0, 'snappy':-1, 'provoking':1, 'bonus':-1, 'upfront':-1, 'purchased':-1,
+'wrote':-1, 'psychoanalytic':0, 'compelled':1, 'guilty':1, 'unclear':1, 'bump':1, 'wins':-1, 'great':-1, 'outsider':1, 'problematic':1, 'substantial':-1, 'agrees':-1, 'conversation':-1, 'boundaries':-1, 'pro':-1, 'lease':0, 'fee':1, 'wanting':0, 'comfortable':-1, 
+'pretending':1, 'mad':1, 'begins':-1, 'timing':0, 'practical':-1, 'detail':0, 'pay':1, 'deals':-1, 'consequences':1, 'therapists':0, 'referring':0, 'desperate':1, 'cousins':0, 'relieved':-1}
+
+
