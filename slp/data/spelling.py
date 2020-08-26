b92d451dc3564ca47cd99494b9a530da91d68c12
Giorgos Paraskevopoulos <georgepar.91@gmail.com>
2020-08-26 14:52:39 +0300

    Add spell checker Data Loader
    Add a class to load the spell checking dataset for S2S model training

---
+++ slp/data/spelling.py
@@ -0,0 +1,32 @@
+import torch
+
+from torch.utils.data import Dataset
+
+from slp.data.transforms import ToTensor
+
+
+class SpellCorrectorDataset(Dataset):
+    def __init__(self, fname, tokenizer=None):
+        self.data = []
+        with open(fname, "r", errors='ignore') as fd:
+            for l in fd:
+                try:
+                    dat = l.strip().split("\t")
+                    if len(dat) == 2 and len(dat[1]) > 2 and len(dat[1]) < 256:
+                        self.data.append(dat)
+                except:
+                    print(l)
+            #self.data = [line.strip().split("\t") for line in fd]
+        lengths = [len(d[0]) for d in self.data]
+        print("Read {} lines".format(len(self.data)))
+        self.tokenizer = tokenizer
+        self.tt = ToTensor(device="cpu", dtype=torch.long)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        src, tgt = self.data[idx]
+        src = self.tt(self.tokenizer(src))
+        tgt = self.tt(self.tokenizer(tgt))
+        return src, tgt
