05f7f38083e7eca925975f704ebdc02d693c037e
efthymisgeo <efthymis.g.georgiou@gmail.com>
2021-01-13 17:23:16 +0200

    added averaging script

---
+++ avg.py
@@ -0,0 +1,22 @@
+import glob
+import os
+import sys
+
+
+if __name__ == "__main__":
+    results_dir = sys.argv[1]
+    results_files = glob.glob(os.path.join(results_dir, "*"))
+    res = {}
+
+    for rf in results_files:
+        with open(rf, "r") as fd:
+            results = [l.strip().split(":") for l in fd]
+            for metric, value in results:
+                value = float(value.strip())
+                if metric in res:
+                    res[metric].append(value)
+                else:
+                    res[metric] = [value]
+
+    for m, values in res.items():
+        print("{}:\t{}".format(m, sum(values)/ len(values)))
