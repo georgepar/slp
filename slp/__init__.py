b35febcb750bdcc8d5c7e35770bcb6069ec323d9
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2021-02-22 14:09:00 +0200

    add logger configuration

--- slp/__init__.py
+++ slp/__init__.py
@@ -1,8 +1,38 @@
+import io
+import logging
+
 from loguru import logger
 from tqdm import tqdm
-import io
 
-# Make loguru play well with tqdm
-logger.remove()
-logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
 
+def configure_logger(logfile_prefix):
+    from slp.util.system import log_to_file
+
+
+    # Intercept standard logging logs in loguru. Should test this for distributed pytorch lightning
+    class InterceptHandler(logging.Handler):
+        def emit(self, record):
+            # Get corresponding Loguru level if it exists
+            try:
+                level = logger.level(record.levelname).name
+            except ValueError:
+                level = record.levelno
+
+            # Find caller from where originated the logged message
+            frame, depth = logging.currentframe(), 2
+            while frame.f_code.co_filename == logging.__file__:
+                frame = frame.f_back
+                depth += 1
+
+            logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+    logger.debug("Intercepting standard logging logs in loguru")
+    logger.debug("This is a side-effect")
+
+    # Make loguru play well with tqdm
+    logger.remove()
+    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
+
+    logging.basicConfig(handlers=[InterceptHandler()], level=0)
+
+    log_to_file(logfile_prefix)
