f6b356f6d672196e1860e861716196acd256b2f2
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2021-02-25 14:21:38 +0200

    Move baseline utils to https://github.com/georgepar/text-baselines

--- tools/transforms/__init__.py
+++
@@ -1,2 +0,0 @@
-# flake8: noqa
-from slp.transforms.util import SklIdentityTransformer, SklComposer
--- tools/transforms/text/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-from slp.transforms.text.skl import (
-    Untokenizer,
-    PunctuationStripper,
-    SplitTokenizer,
-    NltkTokenizer,
-    SpacyTokenizer,
-    NBOWVectorizer,
-)
