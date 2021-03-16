825485606be25c25961b56ea964064cad20644b1
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2021-03-16 21:04:05 +0200

    Add Multimodal collator (for mosi, mosei etc)
    Signed-off-by: Giorgos Paraskevopoulos <geopar@central.ntua.gr>

--- slp/data/collators.py
+++ slp/data/collators.py
@@ -1,6 +1,7 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 import torch
+
 from slp.util.pytorch import mktensor, pad_sequence
 from slp.util.types import Label
 
@@ -115,3 +116,79 @@ class Seq2SeqCollator(object):
         ).to(self.device)
 
         return inputs_padded, targets_padded, lengths_inputs, lengths_targets
+
+
+class MultimodalSequenceClassificationCollator(object):
+    def __init__(
+        self,
+        pad_indx=0,
+        modalities={"visual", "text", "audio"},
+        label_key="label",
+        max_length=-1,
+        label_dtype=torch.float,
+        device="cpu",
+    ):
+        """Collate function for sequence classification tasks
+
+        * Perform padding
+        * Calculate sequence lengths
+
+        Args:
+            pad_indx (int): Pad token index. Defaults to 0.
+            modalities (Set): Which modalities are included in the batch dict
+            max_length (int): Pad sequences to a fixed maximum length
+            label_key (str): String to access the label in the batch dict
+            device (str): device of returned tensors. Leave this as "cpu".
+                The LightningModule will handle the Conversion.
+
+        Examples:
+            >>> dataloader = torch.utils.DataLoader(my_dataset, collate_fn=MultimodalSequenceClassificationCollator())
+        """
+        self.pad_indx = pad_indx
+        self.device = device
+        self.max_length = max_length
+        self.label_key = label_key
+        self.modalities = modalities
+        self.label_dtype = label_dtype
+
+    def extract_sequence(self, batch, key) -> List[torch.Tensor]:
+        return [b[key] for b in batch]
+
+    def __call__(
+        self, batch: List[Dict[str, torch.Tensor]]
+    ) -> Tuple[Dict[str, torch.Tensor], torch.Tensor, Dict[str, torch.Tensor]]:
+        """Call collate function
+
+        Args:
+            batch (List[Dict[str, torch.Tensor]]): Batch of samples.
+                It expects a list of dictionaries from modalities to torch tensors
+
+        Returns:
+            Tuple[Dict[str, torch.Tensor], torch.Tensor, Dict[str, torch.Tensor]]: tuple of
+                (dict batched modality tensors, labels, dict of modality sequence lengths)
+        """
+        inputs = {}
+        lengths = {}
+
+        for m in self.modalities:
+            seq = self.extract_sequence(batch, m)
+            lengths[m] = torch.tensor([s.size(0) for s in seq], device=self.device)
+
+            if self.max_length > 0:
+                lengths[m] = torch.clamp(lengths[m], min=0, max=self.max_length)
+
+            inputs[m] = pad_sequence(
+                seq,
+                batch_first=True,
+                padding_value=self.pad_indx,
+                max_length=self.max_length,
+            ).to(self.device)
+
+        targets: List[Label] = [b[self.label_key] for b in batch]
+
+        # Pad and convert to tensor
+        ttargets: torch.Tensor = mktensor(
+            targets, device=self.device, dtype=self.label_dtype
+        )
+
+        return inputs, ttargets.to(self.device), lengths
