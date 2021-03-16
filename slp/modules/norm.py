29ecc08c6b1c4ae7aae34a206a3c9cb170316b0f
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2021-03-16 13:44:44 +0200

    Add ScaleNorm and safe_norm approximation
    Signed-off-by: Giorgos Paraskevopoulos <geopar@central.ntua.gr>

--- slp/modules/norm.py
+++ slp/modules/norm.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 
 
+def safe_norm(x, eps=1e-5, dim=-1, keepdim=True):
+    return torch.sqrt(torch.sum(torch.square(x), dim=dim, keepdim=keepdim) + eps)
+
+
 class LayerNormTf(nn.Module):
     def __init__(self, hidden_size: int, eps: float = 1e-12):
         """Construct a layernorm module in the TF style (epsilon inside the square root).
@@ -17,7 +21,20 @@ class LayerNormTf(nn.Module):
         u = x.mean(-1, keepdim=True)
         s = (x - u).pow(2).mean(-1, keepdim=True)
         x = (x - u) / torch.sqrt(s + self.variance_epsilon)
+
         return self.weight * x + self.bias
+
+
+class ScaleNorm(nn.Module):
+    def __init__(self, hidden_size: int, eps=1e-5):
+        super(ScaleNorm, self).__init__()
+        self.eps = eps
+        self.g = nn.Parameter(torch.tensor(hidden_size ** 0.5))
+
+    def forward(self, x: torch.Tensor):
+        scaled_norm = self.g / safe_norm(x, dim=-1, keepdim=True).clamp(min=self.eps)
+
+        return scaled_norm * x
 
 
 # Default to pytorch layernorm
